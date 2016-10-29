26c1334ad45337172f532d85581bef5f83924d51
Sullivan Pineau <Sullivan Pineau>
2016-10-29 19:56:15 +0200

    ajout de class question_reponses et generate_list_questions : pour generer les 10 questions/réponses mais revoir récupération entity datastore aléatoire

--- MoviesGame/src/pablo/MadeuxiemeappServlet.java
+++ MoviesGame/src/pablo/MadeuxiemeappServlet.java
@@ -3,6 +3,7 @@ package pablo;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.servlet.http.*;
@@ -28,8 +29,10 @@ public class MadeuxiemeappServlet extends HttpServlet {
 //////////////////////////////////////////////////////////////////////////////////////////
 // mettre à faux pour ajouter des directors mais attention pour l'ajout des directors il ne faut pas actualiser la page 
 //			sinon ils sont ajoutés en double !!!
-			upd.step3(resp,true);
-
+//			upd.step3(resp,true);
+			
+//			generate_list_questions list_questions_reponses = new generate_list_questions(resp);
+//			list_questions_reponses.afficher_tout(resp);
 
 		} catch (Exception e) {
 	    	resp.getWriter().println("UNE ERREUR S'EST PRODUIT !!!!!");
---
+++ MoviesGame/src/pablo/generate_list_questions.java
@@ -0,0 +1,80 @@
+package pablo;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class generate_list_questions {
+	
+	 ArrayList<question_reponses> list_questions_reponses = new ArrayList();
+
+	public generate_list_questions(HttpServletResponse resp) {
+		for(int i =0; i<10; ++i){
+			this.list_questions_reponses.add(new question_reponses(resp));
+		}
+	}
+	
+	public void afficher_tout(HttpServletResponse resp){
+		try{
+			for(int i=0;i<10;++i){
+				if(resp!=null){
+				question_reponses ques_resp = list_questions_reponses.get(i);
+				resp.getWriter().println(" Quand est sortit le film "+ques_resp.getName_film()+" ? ");
+				resp.getWriter().println(" Réponse 1 : "+ques_resp.getReponse_quand_1());
+				resp.getWriter().println(" Réponse 2 : "+ques_resp.getReponse_quand_2());
+				resp.getWriter().println(" Réponse 3 "+ques_resp.getReponse_quand_3());
+
+				resp.getWriter().println(" Qui a produit le film "+ques_resp.getName_film()+" ? ");
+				resp.getWriter().println(" Réponse 1 : "+ques_resp.getReponse_qui_1());
+				resp.getWriter().println(" Réponse 2 : "+ques_resp.getReponse_qui_2());
+				resp.getWriter().println(" Réponse 3 "+ques_resp.getReponse_qui_3());
+				
+				resp.getWriter().println(" Ou a été produit le film "+ques_resp.getName_film()+" ? ");
+
+				resp.getWriter().println(" ------------------------------------------------------------ ");
+				resp.getWriter().println(" --------------------------Réponses-------------------------- ");
+				resp.getWriter().println(" ------------------------------------------------------------ ");
+				resp.getWriter().println(" Le film "+'"'+ques_resp.getName_film()+'"'+" est sortit en "+ques_resp.getReponse_quand_vrai());
+				resp.getWriter().println(ques_resp.getReponse_qui_vrai()+" a produit le film "+'"'+ques_resp.getName_film()+'"');
+				resp.getWriter().println("Le film "+'"'+ques_resp.getName_film()+'"'+" a été produit en longitude : "+ques_resp.getLongitude() + " et en latitude : "+ques_resp.getLatitude()+".");
+
+				resp.getWriter().println(" ------------------------------------------------------------ ");
+				resp.getWriter().println(" ------------------------------------------------------------ ");
+				
+			}else{
+				question_reponses ques_resp = list_questions_reponses.get(i);
+				System.out.println(" Quand est sortit le film "+ques_resp.getName_film()+" ? ");
+				System.out.println(" Réponse 1 : "+ques_resp.getReponse_quand_1());
+				System.out.println(" Réponse 2 : "+ques_resp.getReponse_quand_2());
+				System.out.println(" Réponse 3 "+ques_resp.getReponse_quand_3());
+	
+				System.out.println(" Qui a produit le film "+ques_resp.getName_film()+" ? ");
+				System.out.println(" Réponse 1 : "+ques_resp.getReponse_qui_1());
+				System.out.println(" Réponse 2 : "+ques_resp.getReponse_qui_2());
+				System.out.println(" Réponse 3 "+ques_resp.getReponse_qui_3());
+				
+				System.out.println(" Ou a été produit le film "+ques_resp.getName_film()+" ? ");
+	
+				System.out.println(" ------------------------------------------------------------ ");
+				System.out.println(" --------------------------Réponses-------------------------- ");
+				System.out.println(" ------------------------------------------------------------ ");
+				System.out.println(" Le film "+'"'+ques_resp.getName_film()+'"'+" est sortit en "+ques_resp.getReponse_quand_vrai());
+				System.out.println(ques_resp.getReponse_qui_vrai()+" a produit le film "+'"'+ques_resp.getName_film()+'"');
+				System.out.println("Le film "+'"'+ques_resp.getName_film()+'"'+" a été produit en longitude : "+ques_resp.getLongitude() + " et en latitude : "+ques_resp.getLatitude()+".");
+	
+				System.out.println(" ------------------------------------------------------------ ");
+				System.out.println(" ------------------------------------------------------------ ");
+				}
+			}
+		} catch (Exception e){
+			try{
+				if(resp!=null){
+					resp.getWriter().println("Erreur affichage questions/responses !");
+				}else{
+					System.out.println("Erreur affichage questions/responses !");
+				}
+			} catch (Exception ex){}
+		}
+	}
+
+}
